6308a2fb225dea8bec67e31ad4e43e80865312b5
Dmitrii <bakaevdima4@gmail.com>
2026-05-24 17:43:07 +0300

    got df with all futures and underlying assets

--- utils/get_tickers.py
+++ utils/get_tickers.py
@@ -1,14 +1,47 @@
 import requests
+import pandas as pd
 
 
-def all_futures():
-    url =  "https://iss.moex.com/iss/statistics/engines/futures/markets/forts/series.json"
+def get_all_asset_codes():
+    url = "https://iss.moex.com/iss/statistics/engines/futures/markets/forts/series.json"
 
-    r = requests.get(url)
-    print(r.json()['series']['data'])
+    r = requests.get(url, timeout=10)
+    r.raise_for_status()
 
-def main():
-    all_futures()
+    block = r.json()["series"]
+    df = pd.DataFrame(block["data"], columns=block["columns"])
+    return df["asset_code"].dropna().unique().tolist()
+
+
+def get_futures_by_asset(asset_code: str, show_expired: bool=False):
+    url = "https://iss.moex.com/iss/statistics/engines/futures/markets/forts/series.json"
+    params = {
+        "asset_code": asset_code,
+        "show_expired": int(show_expired)
+    }
+    r = requests.get(url, params=params, timeout=10)
+    r.raise_for_status()
+
+    block = r.json()["series"]
+    df = pd.DataFrame(block["data"], columns=block["columns"])
+    return df
+
+def get_all_futures():
+    asset_codes = get_all_asset_codes()
+
+    dfs = []
+    for code in asset_codes:
+        try:
+            df = get_futures_by_asset(code)
+            if not df.empty:
+                dfs.append(df)
+        except Exception as e:
+            print(e)
+
+    result = pd.concat(dfs, ignore_index=True)
+    return result
+
 
 if __name__ == "__main__":
-    main()
+    df = get_all_futures()
+    print(df)

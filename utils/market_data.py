e55bab71341eec377485feef0a1c95ed0cd7c499
Dmitrii <bakaevdima4@gmail.com>
2026-05-24 16:53:24 +0300

    added better delta

--- utils/market_data.py
+++ utils/market_data.py
@@ -3,14 +3,19 @@ import pandas as pd
 from datetime import datetime, timedelta
 from datetime import date
 
+INTERVAL_DELTA = {
+    1:   timedelta(hours=8),
+    10:  timedelta(days=4),
+    60:  timedelta(days=20),
+    1440: timedelta(days=365),
+}
+
+
 def get_candles(symbol, start_date, end_date, interval=10, show=False, engine="stock", market="shares", board="TQBR"):
     url = f"https://iss.moex.com/iss/engines/{engine}/markets/{market}/boards/{board}/securities/{symbol}/candles.json"
     cur_date = start_date
 
-    if interval == 10:
-        delta = 3
-    else:
-        delta = 0.5
+    delta = INTERVAL_DELTA[interval]
     df = pd.DataFrame()
 
     session = requests.Session()
@@ -18,7 +23,7 @@ def get_candles(symbol, start_date, end_date, interval=10, show=False, engine="s
     while cur_date < end_date:
         params = {
             "from": cur_date,
-            "till": cur_date + timedelta(days=delta),
+            "till": cur_date + delta,
             "interval": interval,
         }
         response = session.get(url, params=params)
@@ -27,7 +32,7 @@ def get_candles(symbol, start_date, end_date, interval=10, show=False, engine="s
         temp_df = pd.DataFrame(data["candles"]["data"], columns=data["candles"]["columns"])
         df = pd.concat([df, temp_df], ignore_index=True)
 
-        cur_date = cur_date + timedelta(days=delta)
+        cur_date = cur_date + delta
         if show:
             print(cur_date)
 
@@ -46,7 +51,7 @@ def main():
         "BRM6",
         date(2026, 5, 20),
         date(2026, 5, 23),
-        interval=10,
+        interval=60,
         engine="futures",
         market="forts",
         board="RFUD",
